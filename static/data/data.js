979f9d4eab2f866a376e194e81c6ffd21c074665
N White <nicolaswhite@yahoo.com>
2019-06-03 17:43:20 -0500

    implements Featured Resource change request

--- static/data/data.js
+++ static/data/data.js
@@ -45,12 +45,19 @@ var DATA = {
     "about": {
         "text": "<p><strong style='font-size:2em;'>Chicago's 1919 race riots<\/strong> barely register in the city\u2019s current consciousness, yet they were a significant turning point in shaping the racial divides we see today. The Newberry Library and 13 partners have organized Chicago 1919: Confronting the Race Riots, a year-long initiative engaging in public conversations about the legacy of the most violent week in Chicago history.<\/p><p>Chicago 1919 is guided by the belief that the 1919 race riots can serve as a lens for understanding Chicago today. Racial tensions related to policing, migration, and housing all came to a head in 1919. By reflecting on the past 100 years, Chicagoans may see how our current racial divisions evolved from the race riots, as the marginalization of African Americans in Chicago became institutionalized through increasingly sophisticated forms of discrimination.<\/p><p>People across Chicago are invited to share in our collective reckoning with a little-known yet tremendously consequential chapter in the city\u2019s history.<\/p>"
     },
+    // "featured": {
+    //     "img": "redsummer_urbanruralmap.jpg",
+    //     "type": "Highlighted Digital Resource",
+    //     "title": "Visualizing the Red Summer",
+    //     "text": "<span>Chicago was not alone in experiencing racial violence in 1919. Outbreaks of racial and labor conflict took place across the country, collectively known as the \u201cRed Summer.\u201d Public historian Karen Sieber has built \u201c<a href='http:\/\/visualizingtheredsummer.com\/' target='_blank'>Visualizing the Red Summer<\/a>,\u201d a website for understanding the conflicts of 1919. This is one of several <a href='.\/resources'>resources<\/a> for further research into the clashes of that year.<\/span>",
+    //     "link": "http:\/\/visualizingtheredsummer.com"
+    // },
     "featured": {
-        "img": "redsummer_urbanruralmap.jpg",
+        "img": "1919map.png",
         "type": "Highlighted Digital Resource",
-        "title": "Visualizing the Red Summer",
-        "text": "<span>Chicago was not alone in experiencing racial violence in 1919. Outbreaks of racial and labor conflict took place across the country, collectively known as the \u201cRed Summer.\u201d Public historian Karen Sieber has built \u201c<a href='http:\/\/visualizingtheredsummer.com\/' target='_blank'>Visualizing the Red Summer<\/a>,\u201d a website for understanding the conflicts of 1919. This is one of several <a href='.\/resources'>resources<\/a> for further research into the clashes of that year.<\/span>",
-        "link": "http:\/\/visualizingtheredsummer.com"
+        "title": "Mapping the 1919 Chicago Riot",
+        "text": "<span>From July 27th to August 3rd, 1919, thousands of black and white Chicagoans fought each other in the streets, resulting in 38 deaths (23 African American and 15 white) and over 500 injuries (two-thirds of them African American). This interactive map shows how the riot was concentrated on the periphery of African American neighborhoods on Chicago’s South Side, as the city’s residential “color line” was both defended and created through inter-racial violence. View <a href='https:\/\/1919map.rcc.uchicago.edu' target='_blank'>Mapping the 1919 Chicago Riot</a>.<\/span>",
+        "link": "https:\/\/1919map.rcc.uchicago.edu"
     },
     "partners": [
         {
